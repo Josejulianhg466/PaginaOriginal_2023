468d07df9c2eedd2a81b4d4cc835c047fc4e6cce
Josejulianhg466 <135545007+Josejulianhg466@users.noreply.github.com>
2023-06-05 00:22:06 -0600

    archivo website 05062023

--- admin/secciones/servicios/index.php
+++ admin/secciones/servicios/index.php
@@ -1,16 +1,28 @@
 <!-- include es de header, ../ es de servicios y el otro ../ es de secciones-->
-<?php include("../../templates/header.php") ?>
+<?php
+include("../../bd.php");
+//SELECCIONAR REGISTROS
+$sentencia = $conexion->prepare("SELECT * FROM `tbl_servicios`");
+$sentencia->execute();
+//ESTA GUARDARÁ TODOS LOS REGISTROS QUE LLEGAN Y ESTÁN DENTRO DE LA TABLA SERVICIO
+//DE ESO SE ENCARGA EL (PDO::FETCH_ASSOC)
+$lista_servicios=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+include("../../templates/header.php")
+
+?>
 
 Listar servicios
 
 <!--bs5-card-head-footer-->
 <div class="card">
     <div class="card-header">
-    <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Agregar registro</a>    
+        <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Agregar registro</a>
+        SERVICIOS
     </div>
     <div class="card-body">
 
-    <!-- bs5-table-defauld-->
+        <!-- bs5-table-defauld-->
         <div class="table-responsive-sm">
             <table class="table">
                 <thead>
@@ -23,17 +35,27 @@ Listar servicios
                     </tr>
                 </thead>
                 <tbody>
+
+                <!--el foreach va a leer la lista de servicios como si fueran registros
+                y con eso podremos ver la información dentro de la tabla-->
+                    <?php foreach($lista_servicios as $registros){ ?>
                     <tr class="">
-                        <td>1</td>
-                        <td>fa-book</td>
-                        <td>Tutoría</td>
-                        <td>Servicios de tutoría de programación</td>
-                        <td>Editar|Eliminar</td>
+                        <td><?php echo $registros['ID'];?></td>
+                        <td><?php echo $registros['icono'];?></td>
+                        <td><?php echo $registros['titulo'];?></td>
+                        <td><?php echo $registros['descripcion'];?></td>
+                        
+                        <td>
+                        <a name="" id="" class="btn btn-info" href="#" role="button">Editar</a>   
+                        |
+                        <a name="" id="" class="btn btn-danger" href="#" role="button">Eliminar</a>
+                        </td>
                     </tr>
+                    <?php }?>
                 </tbody>
             </table>
         </div>
-        
+
     </div>
     <div class="card-footer text-muted">
         <!--seccion vacía-->
